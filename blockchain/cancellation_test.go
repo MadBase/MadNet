0119f4ff7893f1cf12044ea04d3a88a57bad37c5
stuckDaemon <gabriele.bonadiman@gmail.com>
2022-04-20 19:27:10 +0200

    Small typo Added slow return test Modified error log Added TestNewEthereumEndpoint

--- blockchain/cancellation_test.go
+++ blockchain/cancellation_test.go
@@ -32,7 +32,7 @@ func TestSleepWithContextComplete(t *testing.T) {
 	assert.True(t, completed)
 }
 
-func TestSleepWithContextInterupted(t *testing.T) {
+func TestSleepWithContextInterrupted(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 
 	completed := false
@@ -48,10 +48,51 @@ func TestSleepWithContextInterupted(t *testing.T) {
 		}
 		wg.Done()
 	}()
-
 	cancel()
 
 	wg.Wait()
 
 	assert.False(t, completed)
+}
+
+func TestSlowReturn(t *testing.T) {
+	ctx, _ := context.WithCancel(context.Background())
+
+	type args struct {
+		ctx   context.Context
+		delay time.Duration
+		value bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Test slow Return success",
+			args: args{
+				ctx:   ctx,
+				delay: 5000 * time.Millisecond,
+				value: true,
+			},
+			want: true,
+		},
+		{
+			name: "Test slow Return quicker delay",
+			args: args{
+				ctx:   ctx,
+				delay: 500 * time.Millisecond,
+				value: false,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			start := time.Now()
+			assert.Equalf(t, tt.want, blockchain.SlowReturn(tt.args.ctx, tt.args.delay, tt.args.value), "SlowReturn(%v, %v, %v)", tt.args.ctx, tt.args.delay, tt.args.value)
+			elapsed := time.Since(start)
+			assert.GreaterOrEqual(t, elapsed, tt.args.delay, "Delay time has not been respected")
+		})
+	}
 }
