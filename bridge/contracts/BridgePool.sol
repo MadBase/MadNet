9d4cd0c6378214caa660dbd4dc574acaf0c1f006
Gustavo Vázquez Amado <gustavo@madhive.com>
2022-05-09 15:08:14 +0200

    Added processing of MerkleProof as binary

--- bridge/contracts/BridgePool.sol
+++ bridge/contracts/BridgePool.sol
@@ -8,6 +8,8 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "contracts/BToken.sol";
 import "hardhat/console.sol";
 import {BridgePoolErrorCodes} from "contracts/libraries/errorCodes/BridgePoolErrorCodes.sol";
+import "contracts/libraries/parsers/MerkleProofParserLibrary.sol";
+import "contracts/libraries/MerkleProofLibrary.sol";
 
 /// @custom:salt BridgePool
 /// @custom:deploy-type deployStatic
@@ -75,6 +77,34 @@ contract BridgePool is Initializable, ImmutableFactory, MerkleTree {
             merkleRoot == _calculateRootFromMerkleProof(auditPath, 0, merkleKeyHash, leafHash),
             string(abi.encodePacked(BridgePoolErrorCodes.BRIDGEPOOL_PROOF_OF_BURN_NOT_VERIFIED))
         );
+        ERC20(_erc20TokenContract).approve(address(this), burnedUTXO.value);
+        ERC20(_erc20TokenContract).transferFrom(address(this), receiver, burnedUTXO.value);
+    }
+
+    using MerkleProofParserLibrary for bytes;
+    using MerkleProofLibrary for MerkleProofParserLibrary.MerkleProof;
+
+    function withdrawWithBinaryProof(
+        bytes memory encodedMerkleProof,
+        bytes memory encodedBurnedUTXO,
+        bytes32 stateRoot,
+        address receiver
+    ) public onlyFactory {
+        MerkleProofParserLibrary.MerkleProof memory merkleProof = encodedMerkleProof
+            .extractMerkleProof();
+        UTXO memory burnedUTXO = abi.decode(encodedBurnedUTXO, (UTXO));
+        bytes32 leafHash = merkleProof.computeLeafHash2();
+        require(
+            burnedUTXO.owner == receiver,
+            string(
+                abi.encodePacked(BridgePoolErrorCodes.BRIDGEPOOL_RECEIVER_NOT_PROOF_OF_BURN_OWNER)
+            )
+        );
+        require(
+            merkleProof.verifyInclusion(stateRoot),
+            string(abi.encodePacked(BridgePoolErrorCodes.BRIDGEPOOL_PROOF_OF_BURN_NOT_VERIFIED))
+        );
+
         ERC20(_erc20TokenContract).approve(address(this), burnedUTXO.value);
         ERC20(_erc20TokenContract).transferFrom(address(this), receiver, burnedUTXO.value);
     }
