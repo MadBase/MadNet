2b4494a10b7c81d677345ab1bbbb34963ca0ed82
stuckDaemon <gabriele.bonadiman@gmail.com>
2022-05-13 09:30:35 +0200

    Continue CI integration

--- scripts/base-scripts/register_test.sh
+++ scripts/base-scripts/register_test.sh
@@ -7,10 +7,8 @@ BRIDGE_DIR=./bridge
 
 cd $BRIDGE_DIR
 
-
+npx hardhat setHardhatIntervalMining --network dev --interval 300
 npx hardhat --network dev --show-stack-traces registerValidators --factory-address "$@"
 
 
 cd $CURRENT_WD
-
-
