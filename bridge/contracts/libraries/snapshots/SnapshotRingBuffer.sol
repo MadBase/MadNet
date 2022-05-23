69bf564cb3171ab9dfaffdc804b59ed4434808e1
ZJ <zijunlin32@gmail.com>
2022-05-23 19:13:53 -0400

    archived ts-types and artifacts

--- bridge/contracts/libraries/snapshots/SnapshotRingBuffer.sol
+++ bridge/contracts/libraries/snapshots/SnapshotRingBuffer.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT-open-group
-pragma solidity ^0.8.11; 
+pragma solidity ^0.8.11;
 
-import "contracts/libraries/parsers/BClaimsParserLibrary.sol"; 
+import "contracts/libraries/parsers/BClaimsParserLibrary.sol";
 import "contracts/interfaces/ISnapshots.sol";
 import "hardhat/console.sol";
 struct Epoch {
@@ -12,42 +12,51 @@ struct SnapshotBuffer {
     Snapshot[6] _array;
 }
 
-
 library RingBuffer {
-
-    function get(SnapshotBuffer memory self_, uint32 epoch_)
+    function get(SnapshotBuffer storage self_, uint32 epoch_)
         internal
         view
-        returns (Snapshot memory)
+        returns (Snapshot storage)
     {
         return self_._array[_indexFor(self_, epoch_)];
     }
+
     //writes the new snapshot to the buffer
-    function set(SnapshotBuffer storage self_, function (uint32) returns(uint32) epochFor_, Snapshot memory new_)
-        internal
-        returns(uint32)
-    {
-        //get the epoch corresponding to the blocknumber 
+    function set(
+        SnapshotBuffer storage self_,
+        function(uint32) returns (uint32) epochFor_,
+        Snapshot memory new_
+    ) internal returns (uint32) {
+        //get the epoch corresponding to the blocknumber
         uint32 epoch = epochFor_(new_.blockClaims.height);
         //gets the snapshot that was at that location of the buffer
-        Snapshot memory old = self_._array[_indexFor(self_, epoch)];
-        //checks if the new snapshot height is greater than the previous 
+        Snapshot storage old = self_._array[_indexFor(self_, epoch)];
+        //checks if the new snapshot height is greater than the previous
         require(new_.blockClaims.height > old.blockClaims.height, "invalid new blockheight");
         unsafeSet(self_, new_, epoch);
         return epoch;
     }
 
-    function unsafeSet(SnapshotBuffer storage self_, Snapshot memory new_, uint32 epoch_) internal {
+    function unsafeSet(
+        SnapshotBuffer storage self_,
+        Snapshot memory new_,
+        uint32 epoch_
+    ) internal {
         self_._array[_indexFor(self_, epoch_)] = new_;
     }
+
     /**
-    * @dev calculates the congruent value for current epoch in respect to the array length 
-    * for index to be replaced with most recent epoch
-    * @param epoch_ epoch_ number associated with the snapshot 
-    */
-    function _indexFor(SnapshotBuffer memory self_, uint32 epoch_) internal pure returns(uint256) {
-        //TODO determine if this is necessary 
-        // require(epoch_ > 0);
+     * @dev calculates the congruent value for current epoch in respect to the array length
+     * for index to be replaced with most recent epoch
+     * @param epoch_ epoch_ number associated with the snapshot
+     */
+    function _indexFor(SnapshotBuffer storage self_, uint32 epoch_)
+        internal
+        view
+        returns (uint256)
+    {
+        //TODO determine if this is necessary
+        require(epoch_ > 0);
         return epoch_ % self_._array.length;
     }
 }
@@ -57,16 +66,17 @@ library EpochLib {
         self_._value = value_;
     }
 
-    function get(Epoch memory self_) internal view returns(uint32) {
+    function get(Epoch storage self_) internal view returns (uint32) {
         return _max(1, self_._value);
     }
+
     //TODO determine if useful
-    function isZero(Epoch memory self_) internal pure returns(bool) {
+    function isZero(Epoch storage self_) internal view returns (bool) {
         return self_._value == 0;
     }
 
-    function _max(uint32 a, uint32 b) internal pure returns(uint32) {
-        if (a > b ) {
+    function _max(uint32 a, uint32 b) internal pure returns (uint32) {
+        if (a > b) {
             return a;
         }
         return b;
@@ -78,36 +88,38 @@ abstract contract SnapshotRingBuffer {
     using EpochLib for Epoch;
 
     // Must be defined in storage contract
-    function _getEpochFromHeight(uint32) internal virtual view returns(uint32);
+    function _getEpochFromHeight(uint32) internal view virtual returns (uint32);
 
     // Must be defined in storage contract
-    function snapshotsBuffer() internal virtual returns(SnapshotBuffer storage);
+    function snapshotsBuffer() internal virtual returns (SnapshotBuffer storage);
 
     // Must be defined in storage contract
-    function epochReg() internal virtual returns(Epoch storage);
-    
+    function epochReg() internal virtual returns (Epoch storage);
+
     function _setEpoch(uint32 epoch_) internal {
         epochReg().set(epoch_);
     }
+
     function _getEpoch() internal returns (uint32) {
         return epochReg().get();
     }
+
     /**
-    * @notice Assigns the snapshot to correct index and updates __epoch
-    * @param snapshot_ to be stored
-    * @return epoch of the passed snapshot
-    */
-    function _setSnapshot(Snapshot memory snapshot_) internal returns(uint32) {
+     * @notice Assigns the snapshot to correct index and updates __epoch
+     * @param snapshot_ to be stored
+     * @return epoch of the passed snapshot
+     */
+    function _setSnapshot(Snapshot memory snapshot_) internal returns (uint32) {
         uint32 epoch = snapshotsBuffer().set(_getEpochFromHeight, snapshot_);
         epochReg().set(epoch);
         return epoch;
     }
 
     /**
-    * @notice Returns the snapshot for the passed epoch and safety flag
-    * @param epoch_ of the snapshot
-    * @return ok if the struct is valid and the snapshot struct itself
-    */
+     * @notice Returns the snapshot for the passed epoch and safety flag
+     * @param epoch_ of the snapshot
+     * @return ok if the struct is valid and the snapshot struct itself
+     */
     function _getSnapshot(uint32 epoch_) internal returns (bool ok, Snapshot memory snapshot) {
         //get the pointer to the specified epoch snapshot
         Snapshot memory temp = snapshotsBuffer().get(epoch_);
@@ -119,19 +131,21 @@ abstract contract SnapshotRingBuffer {
     }
 
     /**
-    * @notice Returns the snapshot for the passed height and safety flag
-    * @param height_ of the snapshot
-    * @return ok if the struct is valid and the snapshot struct itself
-    */
-    function _getSnapshotForHeight(uint32 height_) internal returns (bool ok, Snapshot memory snapshot) {
+     * @notice Returns the snapshot for the passed height and safety flag
+     * @param height_ of the snapshot
+     * @return ok if the struct is valid and the snapshot struct itself
+     */
+    function _getSnapshotForHeight(uint32 height_)
+        internal
+        returns (bool ok, Snapshot memory snapshot)
+    {
         return _getSnapshot(_getEpochFromHeight(height_));
     }
 
     /**
-    * @return ok if the struct is valid and the snapshot struct itself
-    */
+     * @return ok if the struct is valid and the snapshot struct itself
+     */
     function _getLastSnapshot() internal returns (bool ok, Snapshot memory snapshot) {
         return _getSnapshot(epochReg().get());
     }
 }
-
