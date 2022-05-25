c5da16b99a0289f2e9dcd3a98d8232708960794c
kasvtv <kazaakas@hotmail.com>
2022-05-25 13:39:36 +0200

    fix race condition

--- dynamics/storage.go
+++ dynamics/storage.go
@@ -100,6 +100,9 @@ func (s *Storage) Init(rawDB rawDataBase, logger *logrus.Logger) error {
 // Start allows normal operations to begin. This MUST be called after Init
 // and can only be called once.
 func (s *Storage) Start() {
+	s.Lock()
+	defer s.Unlock()
+
 	s.startOnce.Do(func() {
 		close(s.startChan)
 	})
@@ -111,7 +114,7 @@ func (s *Storage) Start() {
 // to the database
 func (s *Storage) UpdateStorage(txn *badger.Txn, update Updater) error {
 	<-s.startChan
-	
+
 	s.Lock()
 	defer s.Unlock()
 
@@ -163,7 +166,7 @@ func (s *Storage) UpdateStorage(txn *badger.Txn, update Updater) error {
 // Once we find the beginning, we iterate forward and update all forward nodes.
 func (s *Storage) updateStorageValue(txn *badger.Txn, update Updater) error {
 	<-s.startChan
-	
+
 	epoch := update.Epoch()
 	ll, err := s.database.GetLinkedList(txn)
 	if err != nil {
@@ -350,7 +353,7 @@ func (s *Storage) updateStorageValue(txn *badger.Txn, update Updater) error {
 // we modify Storage.
 func (s *Storage) LoadStorage(txn *badger.Txn, epoch uint32) error {
 	<-s.startChan
-	
+
 	s.Lock()
 	defer s.Unlock()
 	rs, err := s.loadStorage(txn, epoch)
@@ -437,7 +440,7 @@ func (s *Storage) loadRawStorage(txn *badger.Txn, epoch uint32) (*RawStorage, er
 // If the node is added at the head, then LinkedList must be updated
 // to reflect this change.
 func (s *Storage) addNode(txn *badger.Txn, node *Node) error {
-	 <-s.startChan
+	<-s.startChan
 
 	// Ensure node.rawStorage and node.thisEpoch are valid;
 	// other parameters should not be set.
@@ -595,7 +598,7 @@ func (s *Storage) addNodeSplit(txn *badger.Txn, node, prevNode, nextNode *Node) 
 // GetMaxBytes returns the maximum allowed bytes
 func (s *Storage) GetMaxBytes() uint32 {
 	<-s.startChan
-	
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetMaxBytes()
@@ -604,7 +607,7 @@ func (s *Storage) GetMaxBytes() uint32 {
 // GetMaxProposalSize returns the maximum size of bytes allowed in a proposal
 func (s *Storage) GetMaxProposalSize() uint32 {
 	<-s.startChan
-	
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetMaxProposalSize()
@@ -613,7 +616,7 @@ func (s *Storage) GetMaxProposalSize() uint32 {
 // GetSrvrMsgTimeout returns the time before timeout of server message
 func (s *Storage) GetSrvrMsgTimeout() time.Duration {
 	<-s.startChan
-	
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetSrvrMsgTimeout()
@@ -622,7 +625,7 @@ func (s *Storage) GetSrvrMsgTimeout() time.Duration {
 // GetMsgTimeout returns the timeout to receive a message
 func (s *Storage) GetMsgTimeout() time.Duration {
 	<-s.startChan
-	
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetMsgTimeout()
@@ -631,7 +634,7 @@ func (s *Storage) GetMsgTimeout() time.Duration {
 // GetProposalStepTimeout returns the proposal step timeout
 func (s *Storage) GetProposalStepTimeout() time.Duration {
 	<-s.startChan
-	
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetProposalStepTimeout()
@@ -640,7 +643,7 @@ func (s *Storage) GetProposalStepTimeout() time.Duration {
 // GetPreVoteStepTimeout returns the prevote step timeout
 func (s *Storage) GetPreVoteStepTimeout() time.Duration {
 	<-s.startChan
-	
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetPreVoteStepTimeout()
@@ -649,7 +652,7 @@ func (s *Storage) GetPreVoteStepTimeout() time.Duration {
 // GetPreCommitStepTimeout returns the precommit step timeout
 func (s *Storage) GetPreCommitStepTimeout() time.Duration {
 	<-s.startChan
-	
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetPreCommitStepTimeout()
@@ -659,7 +662,7 @@ func (s *Storage) GetPreCommitStepTimeout() time.Duration {
 // moving into the DeadBlockRound
 func (s *Storage) GetDeadBlockRoundNextRoundTimeout() time.Duration {
 	<-s.startChan
-	
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetDeadBlockRoundNextRoundTimeout()
@@ -668,7 +671,7 @@ func (s *Storage) GetDeadBlockRoundNextRoundTimeout() time.Duration {
 // GetDownloadTimeout returns the timeout for downloads
 func (s *Storage) GetDownloadTimeout() time.Duration {
 	<-s.startChan
-	
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetDownloadTimeout()
@@ -677,7 +680,7 @@ func (s *Storage) GetDownloadTimeout() time.Duration {
 // GetMinTxFee returns the minimum transaction fee.
 func (s *Storage) GetMinTxFee() *big.Int {
 	<-s.startChan
-	
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetMinTxFee()
@@ -686,7 +689,7 @@ func (s *Storage) GetMinTxFee() *big.Int {
 // GetTxValidVersion returns the transaction valid version
 func (s *Storage) GetTxValidVersion() uint32 {
 	<-s.startChan
-	
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetTxValidVersion()
@@ -695,7 +698,7 @@ func (s *Storage) GetTxValidVersion() uint32 {
 // GetValueStoreFee returns the transaction fee for ValueStore
 func (s *Storage) GetValueStoreFee() *big.Int {
 	<-s.startChan
-	
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetValueStoreFee()
@@ -704,7 +707,7 @@ func (s *Storage) GetValueStoreFee() *big.Int {
 // GetValueStoreValidVersion returns the ValueStore valid version
 func (s *Storage) GetValueStoreValidVersion() uint32 {
 	<-s.startChan
-	
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetValueStoreValidVersion()
@@ -712,8 +715,8 @@ func (s *Storage) GetValueStoreValidVersion() uint32 {
 
 // GetAtomicSwapFee returns the transaction fee for AtomicSwap
 func (s *Storage) GetAtomicSwapFee() *big.Int {
-	 <-s.startChan
-	
+	<-s.startChan
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetAtomicSwapFee()
@@ -722,7 +725,7 @@ func (s *Storage) GetAtomicSwapFee() *big.Int {
 // GetAtomicSwapValidStopEpoch returns the last epoch at which AtomicSwap is valid
 func (s *Storage) GetAtomicSwapValidStopEpoch() uint32 {
 	<-s.startChan
-	
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetAtomicSwapValidStopEpoch()
@@ -731,7 +734,7 @@ func (s *Storage) GetAtomicSwapValidStopEpoch() uint32 {
 // GetDataStoreEpochFee returns the DataStore fee per epoch
 func (s *Storage) GetDataStoreEpochFee() *big.Int {
 	<-s.startChan
-	
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetDataStoreEpochFee()
@@ -740,7 +743,7 @@ func (s *Storage) GetDataStoreEpochFee() *big.Int {
 // GetDataStoreValidVersion returns the DataStore valid version
 func (s *Storage) GetDataStoreValidVersion() uint32 {
 	<-s.startChan
-	
+
 	s.RLock()
 	defer s.RUnlock()
 	return s.rawStorage.GetDataStoreValidVersion()
