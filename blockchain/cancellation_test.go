cd79100924658f4b9b666e20e745a306338c23af
Dan <106091784+madhivedan@users.noreply.github.com>
2022-06-07 11:07:55 -0400

    Touching file to trigger CI

--- blockchain/cancellation_test.go
+++ blockchain/cancellation_test.go
@@ -21,13 +21,12 @@ func TestCancellation_SleepWithContextComplete(t *testing.T) {
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		err := blockchain.SleepWithContext(ctx, SLEEP_DURATION)
 		if err == nil {
 			completed = true
 		}
-		wg.Done()
 	}()
-
 	wg.Wait()
 
 	assert.True(t, completed)
